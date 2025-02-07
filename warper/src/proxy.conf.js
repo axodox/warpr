664d585742d043568e8b04daa0f7ea9b2e84202b
Peter Major <axodox@outlook.com>
2025-02-07 23:00:10 +0100

    Enable https for development

--- warper/src/proxy.conf.js
+++ warper/src/proxy.conf.js
@@ -1,7 +1,7 @@
 const { env } = require('process');
 
-const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
-    env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'https://localhost:7074';
+const target = env.ASPNETCORE_HTTPS_PORT ? `https://0.0.0.0:${env.ASPNETCORE_HTTPS_PORT}` :
+  env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'https://0.0.0.0:7074';
 
 const PROXY_CONFIG = [
   {
---
+++ wormhole/notes.md
@@ -0,0 +1,7 @@
+# Notes
+
+## Convert PFX to PEM
+
+```ps1
+openssl pkcs12 -in axodox-pc.pfx -out axodox-pc.pem -nodes
+```
