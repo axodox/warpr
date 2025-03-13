345fab513b791c1231cd79dbb0b6361694ba156c
Peter Major <axodox@outlook.com>
2025-03-13 21:52:02 +0100

    Added AppVeyor config

--- desk-beamer/desk-beamer.cpp
+++ desk-beamer/desk-beamer.cpp
@@ -1,7 +1,5 @@
 #include "Include\WarpDrive.h"
 
-#include <iostream>
-
 using namespace Warpr;
 using namespace Warpr::Capture;
 using namespace std;
@@ -15,17 +13,6 @@ int main()
 
   WarpDrive drive{ configuration };
 
-  this_thread::sleep_for(10000s);
+  system("pause");
   return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
